8c4a16adabec0944dc00e1f9c8a03b5f1592d9d6
JiansenPanda <jiansen.he@hungrypanda.co>
2018-04-23 23:15:28 +0100

    corrent question number

--- 2018/Java/Q3.java
+++ 2018/Java/Q3.java
@@ -1,30 +1,14 @@
 import java.util.*;
 
-public class Main {
-  static class People {
-    int id;
-    int height;
-    int weight;
-    public People(int id, int weight, int height) {
-      this.id = id;
-      this.height = height;
-      this.weight = weight;
-    }
+public class Main{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int num = s.nextInt();
+    System.out.println(solution(num));
+    scanner.close();
   }
 
-  public static void main(String[] args) {
-      Scanner scan = new Scanner(System.in);
-      int n = scan.nextInt();
-      People[] people = new People[n];
-      for (int i = 0; i < n; ++i) {
-        int id = scan.nextInt();
-        people[i] = new People(id, scan.nextInt(), scan.nextInt());
-      }
-      System.out.println(solution(people));
-      scanner.close();
-  }
-
-  private static int solution(People[] people) {
+  private static long solution(int num) {
 
   }
 }
